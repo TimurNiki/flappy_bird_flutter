66a2a34dcadcad8d70ecd7e4c971c43610ef8819
TimurNiki <timurniki95@gmail.com>
2023-12-26 23:12:17 +0400

    core commit

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Assets{
+const Assets._();
+
+}
---
+++ lib/core/init/init.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Init{
+const Init._();
+static Future <void> init() async
+}
