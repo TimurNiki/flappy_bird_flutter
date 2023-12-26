286b3cb304373f15951156b998413404fa52a462
TimurNiki <timurniki95@gmail.com>
2023-12-26 16:18:10 +0400

    first commit

---
+++ lib/game/flappy_bird_game.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: no_logic_in_create_state
+import 'package:flame/game.dart';
+
+
+class FlappyBirdGame extends FlameGame{
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlappyBirdGame());
+}
+
