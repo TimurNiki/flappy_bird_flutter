64b521a8dd0471a66eea303b8c3d3d4ef0ee61fc
TimurNiki <timurniki95@gmail.com>
2023-12-27 18:16:19 +0400

    core commit

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 @immutable
-class Assets{
-const Assets._();
+class Assets {
+  const Assets._();
+  
+  static const String background = 'assets/images/background.png';
+  static const String ground = 'assets/images/ground.png';
+  static const String clouds = 'assets/images/clouds.png';
+  static const String pipe = 'assets/images/pipe.png';
+  static const pipeRotated = 'assets/images/pipe_rotated.png';
 
+  static const String birdMidFlap = 'assets/images/bird_midflap.png';
+  static const String birdUpFlap = 'assets/images/bird_upflap.png';
+  static const String birdDownFlap = 'assets/images/bird_downflap.png';
+
+  static const String gameOver = 'assets/images/gameover.png';
+  static const String menu = 'assets/images/menu.jpg';
+  static const String message = 'assets/images/message.png';
+
+  static const String flying = 'fly.wav';
+  static const String collision = 'collision.wav';
+  static const String point = 'point.wav';
 }
---
+++ lib/core/constants/game_config.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class GameConfig{
+  const GameConfig._();
+   static const double groundHeight = 110.0;
+  static const double gameSpeed = 200.0;
+  static const double pipeInterval = 1.5;
+  static const double birdVelocity = 210;
+  static const double gravity = -100.0;
+  static const double cloudsHeight = 70.0;
+}
